1efe2101eb6da8f43b9851fcdc099e3c491a409c
João Victor Morais <jv.moraiscb@gmail.com>
2022-06-18 03:51:34 -0300

    Add 3-bits-shifter

--- 3-bits-shifter/top.vhd
+++ 3-bits-shifter/top.vhd
@@ -12,4 +12,16 @@ end top;
 architecture top_arch of top is
     signal prio_mode : std_logic_vector(1 downto 0);
 begin
+    mode_unit : entity work.prio_encoder(prio_encoder_arch)
+        port map(
+            bits_in => mode,
+            bits_out => prio_mode
+        );
+    shifter_unit : entity work.shifter(shifter_arch)
+        port map(
+            mode => prio_mode,
+            bits_in => bits,
+            bits_out => leds
+        );
+    
 end top_arch;
