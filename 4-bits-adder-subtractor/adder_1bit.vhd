b8597babc8b54ebbba3b3c89e5dfb84e3cf18c50
João Victor Morais <jv.moraiscb@gmail.com>
2022-07-13 20:07:09 -0300

    Learning-fsm

--- 4-bits-adder-subtractor/adder_1bit.vhd
+++ 4-bits-adder-subtractor/adder_1bit.vhd
@@ -13,7 +13,7 @@ end adder_1bit;
 
 architecture adder_arch of adder_1bit is
 begin
-    co <= (a and b) or (a and ci) or (b and ci);
+    co <= (a and b) or (ci and (a xor b));
     s <= (a xor b xor ci);
 
 end adder_arch;
